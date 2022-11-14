f3c4909bcfd43b4d9a83a0784fb8a3cffb464064
Marione <mbasaysa@jaumebalmes.net>
2022-11-14 20:50:52 +0100

    layout de marione

---
+++ app/src/main/java/com/example/tascalayout/DosLayouts.java
@@ -0,0 +1,14 @@
+package com.example.tascalayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DosLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dos_layouts);
+    }
+}
